55dad3d43a5eb2cd9f8498eaee25b57900dd4dab
Abdallah <abdallaharar@turnkii.com>
2015-08-28 17:27:26 -0400

    Added index page, and integrated assets; fixed styling for workorder/index page

---
+++ app/assets/javascripts/dash/main.js
@@ -0,0 +1,62 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= $(window).height()){
+            $(".navbar").removeClass('navbar-light');
+						$(".navbar").addClass('navbar-dark');
+        } else{
+					$(".navbar").addClass('navbar-light');
+					$(".navbar").removeClass('navbar-dark');
+				}
+    });
+
+    $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+})
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1140px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 320px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 250);
+			});
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly();
+
+	});
+
+})(jQuery);
